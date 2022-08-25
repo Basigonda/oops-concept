b554a89fa389e08062f1da98c69ce78d84a3e1f7
basigonda <basi@gmail.com>
2022-08-25 09:24:03 +0530

    loop file is added

---
+++ src/com/basi/While_loop.java
@@ -0,0 +1,20 @@
+package com.basi;
+
+public class While_loop {
+	public static void main (String[] argS) {
+		int i=1,limit=100;
+		while(limit<=100);
+		{
+System.out.println(i);
+i=i++;
+
+
+		}
+			
+	
+		
+		
+	}
+	
+
+}
